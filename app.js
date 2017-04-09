b9239348d0025768d96ba4d98fcd4b61d81fbaae
Maria Silla <maria.sylla@gmail.com>
2017-04-09 01:16:21 -0400

    add multer

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//NPM packages
 const express = require('express');
 const app = express();
 const methodOverride = require('method-override')
@@ -5,13 +6,15 @@ const pgp = require('pg-promise')();
 const mustacheExpress = require('mustache-express');
 const bodyParser = require("body-parser");
 const session = require('express-session');
-const imgur = require('imgur-node-api');
 const path = require('path');
 const axios = require('axios');
+const multer  = require('multer');
 
+//Bcrypt
 const bcrypt = require('bcrypt');
 const salt = bcrypt.genSalt(10);
 
+//configuration of packages
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
@@ -20,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride('_method'))
 
+//configuration of session package
 app.use(session({
   secret: 'ART',
   resave: false,
@@ -27,40 +31,19 @@ app.use(session({
   cookie: { secure: false }
 }))
 
+
+//connect to art_port_db database
 var db = pgp('postgres://kuzia@localhost:5432/art_port_db');
 
-
+//render Home page
 app.get('/', function(req, res){
   res.render('home/index');
 });
 
-
-app.get('/profile', function(req, res){
-  let data ;
-  if(req.session.user){
-    data = {
-      "logged_in": true,
-      "email": req.session.user.email,
-      "zipcode": req.session.user.zipcode
-    };
-    var url = 'https://api.meetup.com/2/open_events?key=125567365e1835372f462b14a4a2d41&sign=true&photo-host=public&zip=10003&text=painter&page=20';
-    // var route = 'movie/now_playing?'
-    // var key = '125567365e1835372f462b14a4a2d41';
-
-    axios.get(url)
-    .then(function(response){
-      data.meetup = response.data.results
-    //  console.log(data.meetup);
-    res.render('profile/index', data);
-  })
-  } else {
-    res.render('home/index');
-  }
-})
-
+// User Authentication
 app.post('/login', function(req, res){
-  let data = req.body;
-  let auth_error = "Authorization Failed: Invalid email/password";
+  var data = req.body;
+  var auth_error = "Authorization Failed: Invalid email/password";
 
   db
     .one("SELECT * FROM users WHERE email = $1", [data.email])
@@ -71,7 +54,7 @@ app.post('/login', function(req, res){
       bcrypt.compare(data.password, user.password_digest, function(err, cmp){
         if(cmp){
           req.session.user = user;
-          res.redirect("/profile");
+          res.redirect("/profile/");
         } else {
           res.send(auth_error);
         }
@@ -80,12 +63,37 @@ app.post('/login', function(req, res){
  });
 
 
+ //render Profile page
+ app.get('/profile/', function(req, res){
+   var data ;
+   if(req.session.user){
+     data = {
+       "logged_in": true,
+       "email": req.session.user.email,
+       "zipcode": req.session.user.zipcode,
+       "nickname": req.session.user.nickname
+     };
+     // var url = 'https://api.meetup.com/2/open_events?key=125567365e1835372f462b14a4a2d41&sign=true&photo-host=public&zip=10003&text=painter&page=20';
+     var url = 'https://api.meetup.com/find/events?key=125567365e1835372f462b14a4a2d41&sign=true&photo-host=public&text=painter&page=6';
+     axios.get(url)
+     .then(function(response){
+       data.meetup = response.data
+     //  console.log(data.meetup);
+     res.render('profile/user', data);
+   })
+   } else {
+     res.render('home/index');
+   }
+ })
+
+//render user sign-up
 app.get('/signup', function(req, res){
   res.render('signup/index');
 });
 
+//User sign-up
 app.post('/signup', function(req, res){
-  let data = req.body;
+  var data = req.body;
   bcrypt
     .hash(data.password, 10, function(err, hash){
       db.none(
@@ -101,32 +109,71 @@ app.post('/signup', function(req, res){
     });
 });
 
-// app.post('/painting_upload', function(req, res){
-  // let data = req.body;
-  //
-  // db.none(
-  //   "INSERT INTO paintings (name, description, type, image_url, user_id) VALUES ($1, $2, $3, $4)",
-  //   [data.name, data.description, data.]
-  // )
-  //  db.none(
-  //   "INSERT INTO users (email, nickname, level, favorite_method, password_digest) VALUES ($1, $2, $3, $4, $5)",
-  //   [data.email, data.nickname, data.level, data.method, hash]
-  // ).catch(function(e){
-  //   res.send('Failed to create user: ' + e);
-  // }).then(function(){
-  //   res.send('User created!')
-  // })
-//     console.log(data);
-//     res.send(200);
+//Render portfolio html
+// app.get('/portfolio', function(req, res){
 //
+//
+//   res.render('profile/portfolio');
 // });
+app.get('/portfolio', function(req, res){
+  var data ;
+  if(req.session.user){
+    data = {
+      "logged_in": true,
+      "email": req.session.user.email,
+      "nickname": req.session.user.nickname
+    };
+    res.render('profile/portfolio', data);
+  } else {
+    res.render('home/index');
+  }
+})
 
-app.get('/painting_upload', function(req, res){
 
-  //API KEY 125567365e1835372f462b14a4a2d41
-
-  res.render('painting_upload/index');
+//Multer upload
+// followed this tutorial: https://www.codementor.io/tips/9172397814/setup-file-uploading-in-an-express-js-application-using-multer-js
+// Render painting upload form
+app.get('/new_painting_upload', function(req, res){
+  var data ;
+  if(req.session.user){
+    data = {
+      "logged_in": true,
+      "email": req.session.user.email,
+      "nickname": req.session.user.nickname
+    };
+    res.render('profile/new_painting_upload', data);
+  } else {
+    res.render('home/index');
+  }
 });
+//
+app.post('/new_painting_upload', multer({ dest: './uploads/'}).single('image'), function(req,res){
+    console.log(req.body);
+    console.log(req.file);
+    res.status(204).end();
+    var data = req.body;
+    // var userId
+
+    db.none(
+      "INSERT INTO paintings (name, description, type) VALUES ($1, $2, $3)",
+      [data.name, data.description, data.type]
+    ).catch(function(e){
+      res.send('Failed to upload: ' + e);
+
+    }).then(function(){
+      // res.send('User created!');
+      res.redirect("/portfolio");
+    });
+    // upload(req,res,function(err) {
+    //     if(err) {
+    //         return res.end("Error uploading file.");
+    //     }
+    //     res.end("File is uploaded");
+    // });
+});
+
+
+
 
 // imgur.setClientID('');
 // imgur.upload(path.join(__dirname, 'images/korablik.JPG'), function (err, res) {
@@ -134,24 +181,26 @@ app.get('/painting_upload', function(req, res){
 // });
 
 
+// Update  user info
+// app.put('/user', function(req, res){
+//   db
+//     .none("UPDATE users SET email = $1 WHERE email = $2",
+//       [req.body.email, req.session.user.email]
+//     ).catch(function(){
+//       res.send('Failed to update user.');
+//     }).then(function(){
+//       res.send('User updated.');
+//     });
+// });
 
 
-app.put('/user', function(req, res){
-  db
-    .none("UPDATE users SET email = $1 WHERE email = $2",
-      [req.body.email, req.session.user.email]
-    ).catch(function(){
-      res.send('Failed to update user.');
-    }).then(function(){
-      res.send('User updated.');
-    });
-});
-
+//Logout
 app.get('/logout', function(req, res){
   req.session.user = false;
   res.redirect("/");
 });
 
+// Starting the server
 app.listen(3000, function () {
   console.log('Server running, listening on port 3000!');
 });
--- db/schema.sql
+++ db/schema.sql
@@ -3,28 +3,27 @@ DROP TABLE IF EXISTS paintings CASCADE;
 DROP TABLE IF EXISTS comments CASCADE;
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,c
+  id SERIAL PRIMARY KEY,
   email VARCHAR(255) UNIQUE,
   nickname VARCHAR(255),
   level VARCHAR(255),
   zipcode INTEGER,
   password_digest VARCHAR(255)
-  -- add location attribute
 );
 
 CREATE TABLE paintings (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
-  description VARCHAR(255),
+  description TEXT,
   type VARCHAR(255),
-  image_url VARCHAR(255),
+  image VARCHAR(255),
   likes INTEGER,
   user_id INTEGER REFERENCES users(id)
 );
 
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY,
-  description VARCHAR(255),
+  description TEXT,
   painting_id INTEGER REFERENCES paintings(id),
   user_id INTEGER REFERENCES users(id)
 );
