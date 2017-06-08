b8c034697b460fb13000b1281495f50c0004737a
Maria Silla <maria.sylla@gmail.com>
2017-06-08 11:45:30 -0400

    add font awesome

--- app.js
+++ app.js
@@ -44,11 +44,11 @@ app.use(session({
 
 
 //connect to art_port_db database
-// var db = pgp('postgres://kuzia@localhost:5432/art_port_db');
+var db = pgp('postgres://kuzia@localhost:5432/art_port_db');
 
 //connect to heroku database
-var herokuDb = 'postgres://nswscekfrxvwcd:39d8d75271a263ebe72b27c121817720fe27b8350364eab4794b6d9b98d4f35a@ec2-23-23-234-118.compute-1.amazonaws.com:5432/d408n9430dserl';
-var db=pgp(herokuDb);
+// var herokuDb = 'postgres://nswscekfrxvwcd:39d8d75271a263ebe72b27c121817720fe27b8350364eab4794b6d9b98d4f35a@ec2-23-23-234-118.compute-1.amazonaws.com:5432/d408n9430dserl';
+// var db=pgp(herokuDb);
 
 
 
@@ -310,10 +310,3 @@ app.get('/logout', function(req, res){
 app.listen(process.env.PORT || 3000, function () {
   console.log('Server running, listening on port 3000!');
 });
-
-//set port to 8000
-// var port = process.env.PORT || 8000
-//
-// app.listen(port, function(){
-//   console.log("Server Running {^-^}");
-// })
